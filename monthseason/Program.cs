af6bcd0648f9ba5a25fa9e5000100dda9d764c59
inuse918 <s2018s34@e-mirim.hs.kr>
2020-05-06 13:51:40 +0900

    함수 테스트-문자열에서 숫자만 추출하기

--- monthseason/Program.cs
+++ monthseason/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 namespace monthseason
 {
@@ -10,6 +11,10 @@ namespace monthseason
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("태어난 달을 입력하세요: ");
+            string test = Console.ReadLine();
+            string temp = Regex.Replace(test, @"\D", ""); // \D는 숫자가 아닌 문자열을 뜻함.
+            Console.WriteLine(temp);
         }
     }
 }
