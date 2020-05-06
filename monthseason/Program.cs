4688ffcbe14e8ffda9454b53eabb8e9d8292c160
inuse918 <s2018s34@e-mirim.hs.kr>
2020-05-06 13:57:28 +0900

    사용자에게 달을 입력받아서 계절을 출력

--- monthseason/Program.cs
+++ monthseason/Program.cs
@@ -11,10 +11,42 @@ namespace monthseason
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("태어난 달을 입력하세요: ");
-            string test = Console.ReadLine();
-            string temp = Regex.Replace(test, @"\D", ""); // \D는 숫자가 아닌 문자열을 뜻함.
-            Console.WriteLine(temp);
+            Console.Write("태어난 달을 입력하세요: ");
+            //string test = Console.ReadLine();
+            //string temp = Regex.Replace(test, @"\D", ""); // \D는 숫자가 아닌 문자열을 뜻함.
+            //Console.WriteLine(temp);
+
+            string month = Console.ReadLine();
+            string temp = Regex.Replace(month, @"\D", "");
+            int monthChange = int.Parse(temp);
+
+            switch (monthChange)
+            {
+                case 3:
+                case 4:
+                case 5:
+                    Console.WriteLine("봄에 태어나셨네요");
+                    break;
+                case 6:
+                case 7:
+                case 8:
+                    Console.WriteLine("여름에 태어나셨네요");
+                    break;
+                case 9:
+                case 10:
+                case 11:
+                    Console.WriteLine("가을에 태어나셨네요");
+                    break;
+                case 12:
+                case 1:
+                case 2:
+                    Console.WriteLine("겨울에 태어나셨네요");
+                    break;
+
+                default:
+                    Console.WriteLine("잘못 입력하셨습니다.");
+                    break;
+            }
         }
     }
 }
